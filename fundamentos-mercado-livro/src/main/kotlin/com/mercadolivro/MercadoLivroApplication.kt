1ea5d5347c783e081649fa01202f1e0bbdefc13b
Cleiton Ortega <c.ortega200935@gmail.com>
2025-07-29 16:50:03 -0300

    feat:Classes, Objetos e Construtores

---
+++ fundamentos-mercado-livro/src/main/kotlin/com/mercadolivro/MercadoLivroApplication.kt
@@ -0,0 +1,19 @@
+package com.mercadolivro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MercadoLivroApplication
+
+class Livro(var titulo: String, var qtdPag: Int){
+	override fun toString(): String {
+		return "Class: Livro. Titulo: ${titulo}, Quantidade de Paginas: ${qtdPag}"
+	}
+}
+
+fun main(args: Array<String>) {
+	//runApplication<MercadoLivroApplication>(*args)
+	var newLivro = Livro("Livro livroso", 100)
+	println(newLivro)
+}
